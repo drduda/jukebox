4d86a76be4351581a897c913f864638e5081284b
drduda <1995marko@gmail.com>
2021-04-20 17:54:23 +0200

    Codebooks are now retrieved

--- genre_classification/sandbox.py
+++ genre_classification/sandbox.py
@@ -24,31 +24,62 @@ from sklearn.multiclass import OneVsRestClassifier
 
 import fma.utils as utils
 
+# Jukebox imports
+from jukebox.hparams import Hyperparams
+from jukebox.make_models import make_model
+import fire
+import torch
+
 AUDIO_DIR = "data/fma_small"
 
-# Load metadata
-tracks = utils.load('data/fma_metadata/tracks.csv')
-subset = tracks.index[tracks['set', 'subset'] <= 'small']
-tracks = tracks.loc[subset]
+def run(model, **kwargs):
+    # Load metadata
+    tracks = utils.load('data/fma_metadata/tracks.csv')
+    subset = tracks.index[tracks['set', 'subset'] <= 'small']
+    tracks = tracks.loc[subset]
 
-assert subset.isin(tracks.index).all()
+    assert subset.isin(tracks.index).all()
 
-# Get split
-train = tracks.index[tracks['set', 'split'] == 'training']
-val = tracks.index[tracks['set', 'split'] == 'validation']
-test = tracks.index[tracks['set', 'split'] == 'test']
-print('{} training examples, {} validation examples, {} testing examples'.format(*map(len, [train, val, test])))
+    # Get split
+    train = tracks.index[tracks['set', 'split'] == 'training']
+    val = tracks.index[tracks['set', 'split'] == 'validation']
+    test = tracks.index[tracks['set', 'split'] == 'test']
+    print('{} training examples, {} validation examples, {} testing examples'.format(*map(len, [train, val, test])))
 
 
-# Get labels
-labels_onehot = LabelBinarizer().fit_transform(tracks['track', 'genre_top'])
-labels_onehot = pd.DataFrame(labels_onehot, index=tracks.index)
+    # Get labels
+    labels_onehot = LabelBinarizer().fit_transform(tracks['track', 'genre_top'])
+    labels_onehot = pd.DataFrame(labels_onehot, index=tracks.index)
 
-# Load raw audio
-#todo what about sampling rate
-loader = utils.LibrosaLoader(sampling_rate=22100)
-SampleLoader = utils.build_sample_loader(AUDIO_DIR, labels_onehot, loader)
-print('Dimensionality: {}'.format(loader.shape))
-train_loader = SampleLoader(train, batch_size=64)
-batch = train_loader.__next__()
-pass
+    # Load raw audio
+    #todo what about sampling rate
+    loader = utils.LibrosaLoader(sampling_rate=22100)
+    SampleLoader = utils.build_sample_loader(AUDIO_DIR, labels_onehot, loader)
+    print('Dimensionality: {}'.format(loader.shape))
+    train_loader = SampleLoader(train, batch_size=5)
+    input, labels = train_loader.__next__()
+
+    # Get models
+    hps = Hyperparams(**kwargs)
+    #sample_hps = Hyperparams(dict(mode=mode, codes_file=codes_file, audio_file=audio_file, prompt_length_in_seconds=prompt_length_in_seconds))
+    device = torch.device("cuda")
+    vqvae, priors = make_model(model, device, hps)
+
+    # Top raw to tokens is the compressing rate
+    top_raw_to_tokens = priors[-1].raw_to_tokens
+    # 8192 context codebooks/(44100 sample rate/128 top_raw_tokens) = 24sec
+
+    # Reshape input
+    input = np.expand_dims(input, axis=-1)
+    # Get codebooks
+    zs = priors[-1].encode(input, start_level=0, end_level=len(priors), bs_chunks=input.shape[0])
+
+    # finetune the top prior (priors[-1]) for classification
+
+    priors[-1].train()
+
+    print("done")
+
+
+if __name__ == '__main__':
+    fire.Fire(run)
